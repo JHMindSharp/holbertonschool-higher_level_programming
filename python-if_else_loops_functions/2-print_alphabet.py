609d5f0a4910f4569fc5031b3adcdc60b9b39c45
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-01-23 20:52:02 +0100

    -m

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-alphabet = ''
-for i in range(ord('a'), ord('z') + 1):
-    alphabet += chr(i)
-print(alphabet, end='')
+for letter in range(97, 123):
+    print("{}" .format(chr(letter)), end="")
+    
