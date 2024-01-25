5ecd4be622964602e1e3885a818cdea0e2caaf0c
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-01-26 00:19:36 +0100

    task 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+arguments = sys.argv
+
+if len(arguments) == 1:
+    num_args = "0 arguments"
+    args_list = ""
+else:
+    num_args = "{} argument{}".format(len(arguments) - 1, "s"
+                                      if len(arguments) > 2 else "")
+    args_list = "\n" + "\n".join(["{}: {}".format(i, arg) for i,
+                                  arg in enumerate(arguments[1:], start=1)])
+
+print("{}{}{}".format(num_args, "." if len(arguments) == 1
+                      else ":", args_list))
