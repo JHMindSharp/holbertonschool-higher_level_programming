1f5a4b4c89b124b3ac1d92c614f7f5e7d847a738
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-01-25 23:06:09 +0100

    task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-add = __import__('add_0').add
+from add_0 import add
+
+def main():
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
+    
+if __name__ == '__main__':
+    main()
