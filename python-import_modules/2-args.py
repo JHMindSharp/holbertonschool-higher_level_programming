703f3a9b33ff901e6caf79cf737734efbcdbe759
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-01-26 00:50:55 +0100

    -m

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,16 +3,21 @@ import sys
 
 
 def main():
-    len_arg = len(sys.argv) - 1
-    if len_arg == 0:
-        print("{} arguments.".format(len_arg))
-    elif len_arg == 1:
-        print("{} arguments:".format(len_arg))
+    arguments = sys.argv
+    if len(arguments) == 1:
+        num_args = "0 arguments"
+        args_list = ""
     else:
-        print("{} arguments:".format(len_arg))
+        num_args = "{} argument{}".format(
+            len(arguments) - 1, "s" if len(arguments) > 2 else ""
+        )
+        args_list = "\n" + "\n".join(
+            ["{}: {}".format(i, arg) for i, arg in enumerate(arguments[1:],
+                                                             start=1)]
+        )
 
-    for i, arg in enumerate(sys.argv[1:], start=1):
-        print("{}: {}".format(i, arg))
+    print("{}{}{}".format(num_args, "." if len(arguments) == 1 else ":",
+                          args_list))
 
 
 if __name__ == "__main__":
