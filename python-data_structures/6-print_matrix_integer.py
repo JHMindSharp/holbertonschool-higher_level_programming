9d2d6e2d592d0d7f8f8d38ec22e0720773ecfb06
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-01-29 12:31:36 +0100

    maj task 6 for space before the last element of the array

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for element in matrix:
-        for i in element:
-            print("{:d}".format(i), end=" ")
+        for i, elem  in enumerate(element):
+            if i < len(element) -1:
+                print("{:d}".format(elem), end=" ")
+            else:
+                print("{:d}".format(elem), end="")
         print()
