4e4b24f6226311d92877ff906e3d426ca043de8d
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-01-29 12:34:03 +0100

    task 6 pycodes

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for element in matrix:
-        for i, elem  in enumerate(element):
-            if i < len(element) -1:
+        for i, elem in enumerate(element):
+            if i < len(element) - 1:
                 print("{:d}".format(elem), end=" ")
             else:
                 print("{:d}".format(elem), end="")
