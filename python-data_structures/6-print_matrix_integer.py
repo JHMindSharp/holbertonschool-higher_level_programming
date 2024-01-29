0c527794da0d8db5f80cb2e8bbe8826bb36380c2
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-01-29 12:21:55 +0100

    task 6 by str format and not using join

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for actual_element in matrix:
-        for i in range(len(actual_element)):
-            element = actual_element[i]
-            actual_element[i] = element
-    for actual_element in matrix:
-        print(" ".join(map(str, actual_element)))
+    for element in matrix:
+        for i in element:
+            print("{:d}".format(i), end=" ")
+        print()
