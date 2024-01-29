e8313a52128a1d2299c3c6b9f3ab719525534365
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-01-29 15:48:56 +0100

    modify task 11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,9 +1,5 @@
+#!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    # Vérifier si l'indice est valide (ni négatif ni en dehors des limites)
-    if idx < 0 or idx >= len(my_list):
-        return my_list  # Renvoyer la liste inchangée si l'indice est invalide
-
-    # Créer une nouvelle liste sans l'élément à l'indice idx
-    new_list = my_list[:idx] + my_list[idx + 1:]
-
-    return new_list  # Renvoyer la nouvelle liste sans l'élément supprimé
+    if 0 <= idx < len(my_list):
+        del my_list[idx]
+    return my_list
