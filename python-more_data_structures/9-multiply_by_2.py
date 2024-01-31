38e593fbd04d73d974dab581f5d90edf47933ef2
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-01-31 18:22:32 +0100

    task 9

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    
+    sec_dictionary = {}
+    for key in a_dictionary:
+        sec_dictionary[key] = a_dictionary[key] * 2
+    return sec_dictionary
