734c3eeaadfbb8dfb34604ba9bfa23269d814e82
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-01-31 22:45:32 +0100

    task 12 maj of none and M for 1000

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_dictionary = {'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    roman_dictionary = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10,
+                        'V': 5, 'I': 1}
     total = 0
     prev_value = 0
 
