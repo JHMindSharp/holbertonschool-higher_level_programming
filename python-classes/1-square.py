ab3086f1ac5fc35947bf4ce6e1e1112b628c2509
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-02-05 02:08:38 +0100

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module to define a square based on 0-square.py"""
+
+
+class Square:
+    """Define a square class of attribute 'size'"""
+    def __init__(self, size):
+        """Initialisation of Square class with attribut 'size'
+        
+        Args:
+        size: The size of the square."""
+        self.__size = size
