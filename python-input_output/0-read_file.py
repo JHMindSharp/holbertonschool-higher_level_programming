49cb7709e9709ae8ff2542dc527f9591ae646a58
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-02-15 15:02:13 +0100

    -m

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python3
-"""Module reading a text file and prints it to stdout"""
+"""
+This module defines the read_file function. It reads the content of a text file
+and prints it to stdout, ensuring proper file closure after reading.
+"""
 
 
 def read_file(filename=""):
+    """
+    Reads and prints the content of a text file to stdout.
+
+    Opens the specified file in read-only mode and prints its content.
+    Uses a context manager to ensure automatic file closure.
+
+    Parameters:
+    - filename (str): Path to the file. Defaults to an empty string, which
+      will result in an error since no file name is specified.
+
+    Returns:
+    - None
+    """
     with open(filename, "r") as file:
         print(file.read())
