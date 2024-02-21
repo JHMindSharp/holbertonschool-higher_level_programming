5b45c90b9391e41e02dfb8ef771d010ff12e5ef4
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-02-21 13:10:25 +0100

    task 4

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -145,3 +145,12 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """
+        Get the area of the rectangle.
+
+        Returns:
+        int: The area of the rectangle.
+        """
+        return self.__height * self.width
--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -57,6 +57,17 @@ class TestRectangle(unittest.TestCase):
             r.x = -5
             r.y = -10
 
+    def test_area(self):
+        """Test the area calculation."""
+        r1 = Rectangle(3, 2)
+        self.assertEqual(r1.area(), 6)
+
+        r2 = Rectangle(2, 10)
+        self.assertEqual(r2.area(), 20)
+
+        r3 = Rectangle(8, 7, 0, 0, 12)
+        self.assertEqual(r3.area(), 56)
+
 
 if __name__ == '__main__':
     unittest.main()
