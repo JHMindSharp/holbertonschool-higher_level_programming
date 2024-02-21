772e1431e4c1d4b6449cf84f12e1adeb5250434a
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-02-21 13:39:29 +0100

    -m

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -159,4 +159,4 @@ class Rectangle(Base):
         """Prints in stdout the Rectangle instance with the character #"""
         print("\n" * self.__y, end="")
         for row in range(self.__height):
-            print("" * self.__x + "#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
