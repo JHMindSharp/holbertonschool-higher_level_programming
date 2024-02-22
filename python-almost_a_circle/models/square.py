1be56e4380626fc2ed6b7db34bc6c894fa6e66c4
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-02-22 12:51:05 +0100

    task 11

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -4,7 +4,7 @@ from .rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """_summary_
+    """ Definit a Square inherits from Rectangle.
 
     Args:
         Rectangle (_type_): _description_
@@ -21,7 +21,26 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """_summary_
+        """Return de format square
         """
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x,
                                                  self.y, self.width)
+
+    @property
+    def size(self):
+        """Get the size of the square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square.
+
+        Args:
+            value: The new size of the square.
+
+        Raises:
+            TypeError: If 'value' is not an integer.
+            ValueError: If 'value' is less than or equal to 0.
+        """
+        self.width = value
+        self.height = value
