3f3d37e9be07585dd97ad2308e10c2ac48a4f33b
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-02-22 21:42:10 +0100

    task 18 modif

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -51,12 +51,14 @@ class Base:
     def create(cls, **dictionary):
         """Returns an instance with all attributes already set."""
         if cls.__name__ == "Rectangle":
-            dummy = cls(1, 1)
+            dummy_instance = cls(1, 1)
         elif cls.__name__ == "Square":
-            dummy = cls(1)
+            dummy_instance = cls(1)
         else:
-            dummy = cls()
-        return dummy
+            dummy_instance = cls()
+
+        dummy_instance.update(**dictionary)
+        return dummy_instance
 
     @classmethod
     def load_from_file(cls):
