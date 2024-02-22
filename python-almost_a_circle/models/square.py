3dc1d15aa19980e02f8e8e10532898ca6b4e5bec
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-02-22 21:02:58 +0100

    task 14

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -56,3 +56,8 @@ class Square(Rectangle):
         elif kwargs:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of a Rectangle."""
+        return {'id': self.id, 'width': self.width,
+                'height': self.height, 'x': self.x, 'y': self.y}
