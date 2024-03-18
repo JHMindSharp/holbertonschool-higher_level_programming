1a2eaea6a21b457067cf30b57627f1acbc10de20
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-03-18 14:13:26 +0100

    task 5/6/7/8/9/10

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,2 @@
--- Script to print the description of the table first_table without using DESCRIBE or EXPLAIN statements
-USE hbtn_0c_0;
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'first_table';
+-- Description of first_table
+SHOW CREATE TABLE first_table;
